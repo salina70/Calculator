86f16154f270059eba5985cd3695ded5afa7b791
salina70 <salinamainali700@gmail.com>
2026-02-17 22:36:37 +0545

    simple calculator

---
+++ app.js
@@ -0,0 +1,30 @@
+let display = document.querySelector(".display");
+let buttons = document.querySelectorAll("button");
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+let output;
+
+clear.addEventListener("click", function () {
+  window.location.reload();
+});
+
+buttons.forEach((btn) => {
+  btn.addEventListener("click", function () {
+    if (
+      !(this.classList.contains("equal") || this.classList.contains("clear"))
+    ) {
+      if (display.textContent === "0" || display.textContent === output) {
+        display.textContent = "";
+        display.textContent = this.textContent;
+      } else {
+        display.textContent += this.textContent;
+      }
+    }
+  });
+});
+
+equal.addEventListener("click", function () {
+  click.play();
+  display.textContent = eval(display.textContent);
+  output = display.textContent;
+});
